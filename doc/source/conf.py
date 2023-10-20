61c16b6e062724a39eb87fa05d798e2cbfa0ce95
Taher Chegini <cheginit@gmail.com>
2023-10-19 20:46:44 -0400

    DOC: Use the correct logo.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -200,7 +200,7 @@ html_theme_options = {
     # "twitter_url": "https://twitter.com/PyData",
     "logo": {
         "text": "CurviRiver",
-        "image_dark": "_static/logo.svg",
+        "image_dark": "_static/logo.png",
         "alt_text": "CurviRiver",
     },
     "use_edit_page_button": True,
